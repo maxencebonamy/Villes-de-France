d1c0e5c8a10b48a76b5c59c2d582f9eb643000a5
Maxence Bonamy <maxencebonamy@gmail.com>
2024-05-10 22:24:02 +0200

    chore: Update max-sm breakpoint in game panel component

--- src/lib/providers/tanstack-query/tanstack-query.provider.tsx
+++ src/lib/providers/tanstack-query/tanstack-query.provider.tsx
@@ -5,7 +5,13 @@ import * as React from "react"
 import { useState, type PropsWithChildren, type ReactElement } from "react"
 
 export const TanstackQueryProvider = ({ children }: PropsWithChildren): ReactElement => {
-	const [client] = useState(new QueryClient())
+	const [client] = useState(new QueryClient({
+		defaultOptions: {
+			queries: {
+				refetchOnWindowFocus: false
+			}
+		}
+	}))
 
 	return (
 		<QueryClientProvider client={client}>
