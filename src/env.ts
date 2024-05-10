d5ebc5577b1cc7766543671e8cb37f7153d79a78
Maxence Bonamy <maxencebonamy@gmail.com>
2024-05-10 22:31:26 +0200

    chore: Update map style URLs with new API key

--- src/env.ts
+++ src/env.ts
@@ -5,11 +5,9 @@ export const env = createEnv({
 	server: {
 	},
 	client: {
-		NEXT_PUBLIC_CITIES_API: z.string().url(),
-		NEXT_PUBLIC_MAPTILER_API: z.string()
+		NEXT_PUBLIC_CITIES_API: z.string().url()
 	},
 	runtimeEnv: {
-		NEXT_PUBLIC_CITIES_API: process.env.NEXT_PUBLIC_CITIES_API,
-		NEXT_PUBLIC_MAPTILER_API: process.env.NEXT_PUBLIC_MAPTILER_API
+		NEXT_PUBLIC_CITIES_API: process.env.NEXT_PUBLIC_CITIES_API
 	}
 })
--- src/lib/components/organisms/map/map.tsx
+++ src/lib/components/organisms/map/map.tsx
@@ -5,11 +5,9 @@ import type { MapProps } from "."
 import MapLibre from "react-map-gl/maplibre"
 import "maplibre-gl/dist/maplibre-gl.css"
 import { useTheme } from "next-themes"
-import { env } from "@/env"
 
 export const Map = ({ children, style, onClick }: MapProps): ReactElement => {
 	const { theme } = useTheme()
-	const MAPTILER_KEY = env.NEXT_PUBLIC_MAPTILER_API
 
 	return (
 		<MapLibre
@@ -19,7 +17,7 @@ export const Map = ({ children, style, onClick }: MapProps): ReactElement => {
 				zoom: 5.3
 			}}
 			styleDiffing
-			mapStyle={`/map-style${theme === "dark" ? "-dark" : ""}.json?key=${MAPTILER_KEY}`}
+			mapStyle={`/map-style${theme === "dark" ? "-dark" : ""}.json`}
 			minZoom={1}
 			onClick={onClick}
 			cursor="crosshair"
