ff44178a7fececa288b803bdd4bd14803fad240b
Maxence Bonamy <maxencebonamy@gmail.com>
2024-05-12 00:59:44 +0200

    chore: Disable touch zoom and rotate on map component

--- src/lib/components/organisms/map/map.tsx
+++ src/lib/components/organisms/map/map.tsx
@@ -26,6 +26,7 @@ export const Map = ({ children, style, onClick }: MapProps): ReactElement => {
 			onRotateEnd={e => e.originalEvent?.preventDefault()}
 			onRotateStart={e => e.originalEvent?.preventDefault()}
 			dragRotate={false}
+			touchZoomRotate={false}
 		>
 			{Array.isArray(children) ? children.filter(Boolean) : children}
 		</MapLibre>
